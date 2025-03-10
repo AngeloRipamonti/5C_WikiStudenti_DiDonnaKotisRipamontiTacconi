1cc3971e06db82c30ebd5e9ed81d6f2ef3caecd2
Alexandros Kotis <kotisalexandros@itis-molinari.eu>
2025-03-10 14:56:57 +0100

    ore/persona fix

--- src/index.php
+++ src/index.php
@@ -1,2 +1,13 @@
 <!-- Scribbler Template -->
 <!-- https://tympanus.net/codrops/2018/01/12/freebie-scribbler-website-template-html-sketch/ -->
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+ </head>
+ <body>
+    ciao 
+ </body>
+ </html>
