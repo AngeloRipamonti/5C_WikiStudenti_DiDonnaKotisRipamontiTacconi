f0d4658665f8f37e604bc5e0bd9a3d169ef5c996
Alexandros Kotis <kotisalexandros@itis-molinari.eu>
2025-04-01 14:13:05 +0200

    user.js

---
+++ src/components/user.js
@@ -0,0 +1,41 @@
+export function user(pubSub) {
+
+    //in base al ruolo ha determinati metodi
+    let role;
+    
+    return{
+        getRole: function(){
+
+        },
+        approveVersion : function(){
+            
+        },
+        approveUser: function(){
+            
+        },
+        approveContent: function(){
+            
+        },
+        rejectVersion: function(){
+            
+        },
+        rejectUser: function(){
+            
+        },
+        rejectContent: function(){
+            
+        },
+        setUsername: function(){
+            
+        },
+        setEmail: function(){
+            
+        },
+        setPassword: function(){
+            
+        },
+        renderAccount: function(){
+            
+        }
+    }
+}
