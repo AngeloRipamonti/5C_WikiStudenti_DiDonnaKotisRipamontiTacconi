c778e4d65499c8ceeb918803f823107390993684
Alexandros Kotis <kotisalexandros@itis-molinari.eu>
2025-04-12 12:28:29 +0200

    searchBar with pubSub

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,6 +1,4 @@
-export const searchBarComponent = (parentElement) => {
-    let onSearch = null;
-
+export const searchBarComponent = (parentElement, PubSub) => {
     return {
         render: () => {
             return new Promise((resolve, reject) => {
@@ -8,18 +6,17 @@ export const searchBarComponent = (parentElement) => {
                     const html = `
                         <label for="table-search" class="sr-only">Search</label>
                         <div class="relative">
-                            <div class="absolute inset-y-0 left-0 flex items-center ps-3 pointer-events-none">
-                            </div>
                             <input type="text" id="table-search" class="form-input bg-light border radius-md width-80" placeholder="Search for items">
                             <button id="search-table" type="button"><i class="fa fa-search"></i></button>
                         </div>
                     `;
-
                     parentElement.innerHTML = html;
 
                     document.getElementById("search-table").onclick = () => {
-                        const input = document.getElementById("table-search");
-                        if (onSearch) onSearch(input.value.trim());
+                        const value = document.getElementById("table-search").value.trim();
+                        if (!value) return;
+
+                        PubSub.publish("search", value);
                     };
 
                     resolve(html);
@@ -27,11 +24,6 @@ export const searchBarComponent = (parentElement) => {
                     reject(e);
                 }
             });
-        },
-
-        // Permette di assegnare il comportamento alla ricerca
-        onSearch: (callback) => {
-            onSearch = callback;
         }
     };
 };
