12629fa2c071c6d01cb420472da3e6dacb9e6c91
Angelo <ripamontiangelo@itis-molinari.eu>
2025-04-29 12:04:03 +0200

    sm fix lens

--- src/index.php
+++ src/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="it">
     <head>
       <meta charset="utf-8">
       <meta name="description" content="">
@@ -11,9 +11,7 @@
       <link rel="stylesheet" href="/assets/css/scribbler-global.css">
       <link rel="stylesheet" href="/assets/css/scribbler-landing.css">
       <link rel="stylesheet" href="/assets/css/home.css">
-
-      <link rel="author" href="humans.txt">
-
+      <link rel="author" href="author.txt">
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
       <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.min.js" integrity="sha384-VQqxDN0EQCkWoxt/0vsQvZswzTHUVOImccYmSyhJTp7kGtPed0Qcx8rK9h9YEgx+" crossorigin="anonymous"></script>
