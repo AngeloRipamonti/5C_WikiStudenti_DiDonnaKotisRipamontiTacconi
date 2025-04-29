5ea1e52c445e97bf04f5ff08d2be7d1fd472ce34
Angelo <ripamontiangelo@itis-molinari.eu>
2025-04-29 10:24:03 +0200

    d file

--- src/login.js
+++
@@ -1,4 +0,0 @@
-import { generateLoginComponent } from "./components/formLogin.js";
-const login = generateLoginComponent(document.querySelector("#login"));
-login.build("token", "privateClass");
-login.renderForm();
--- src/register.js
+++
@@ -1,3 +0,0 @@
-import { generateLoginComponent } from "./components/formLogin.js";
-const register = generateLoginComponent(document.querySelector("#register"));
-register.renderFormRegister();
